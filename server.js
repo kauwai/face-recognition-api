528729fe82aecd6fae9922248cb3c4c62803a359
kauwai <53876126+kauwai@users.noreply.github.com>
2021-02-10 14:40:43 -0300

    Change from host to connectionString

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { USER, PASSWORD, DATABASE, PORT } = process.env;
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
